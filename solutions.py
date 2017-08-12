5f670824eebf41e7ca2c9ae77844cfeb376dbf72
xiaowang <wangxiaonku@gmail.com>
2017-08-12 00:48:51 -0400

    feat: update for question2, complete question2 method

--- solutions.py
+++ solutions.py
@@ -2,17 +2,44 @@
 	then the function returns True. Your function definition should look like: question1(s, t) and return a boolean True or False.
 """
 def question1(s, t):
+	return
 
 
 """ Given a string a, find the longest palindromic substring contained in a. Your function definition should look like question2(a), 
 	and return a string.
 """
 def question2(a):
-
+	expanded_str = "$#"
+	for char in a:
+		expanded_str += char + '#'
+	print(expanded_str)
+	n = len(a)
+	p = [0]*n
+	mx = 0
+	idx = 0
+	resLen = 0
+	resCenter = 0
+	for i in range(1,n):
+		if mx > i:
+			p[i] = min(p[2*idx-i], mx-i)
+		else:
+			p[i] = 1
+		while expanded_str[i+p[i]] == expanded_str[i-p[i]]:
+			p[i]+=1
+		if (mx < i+p[i]):
+			mx = i+p[i]
+			idx = i
+		if (resLen < p[i]):
+			resLen = p[i]
+			resCenter = i
+	print(resCenter)
+	print(resLen)
+	return a[(resCenter-resLen)/2:(resCenter+resLen)/2]
 """Given an undirected graph G, find the minimum spanning tree within G. A minimum spanning tree connects all vertices in a graph 
 	with the smallest possible total weight of edges. Your function should take in and return an adjacency list structured.
 """
 def question3(G):
+	return
 
 """Find the least common ancestor between two nodes on a binary search tree. The least common ancestor is the farthest node from the root 
 	that is an ancestor of both nodes. For example, the root is a common ancestor of all nodes on the tree, but if both nodes are descendents 
@@ -22,9 +49,15 @@ def question3(G):
 	non-negative integer representing the root, and n1 and n2 are non-negative integers representing the two nodes in no particular order.
 """
 def question4(T, r, n1, n2):
+	return
 
 
 """ Find the element in a singly linked list that's m elements from the end. For example, if a linked list has 5 elements, the 3rd element 
 	from the end is the 3rd element. The function definition should look like question5(ll, m), where ll is the first node of a linked list and m is the "mth number from the end". You should copy/paste the Node class below to use as a representation of a node in the linked list. Return the value of the node at that position.
 """
 def question5(ll, m):
+	return
+
+
+
+print question2("12212")
